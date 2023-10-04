a4baeb65d5d4c57d9736b799ecf2b932736b9f5f
Allan Nava <allannava95@gmail.com>
2023-10-04 09:46:55 +0200

    first commit

---
+++ build-ubuntu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+apt-get -y install build-essential curl tar pkg-config
+apt-get -y --force-yes install \
+  wget \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  frei0r-plugins-dev \
+  gawk \
+  libass-dev \
+  libfreetype6-dev \
+  libopencore-amrnb-dev \
+  libopencore-amrwb-dev \
+  libsdl1.2-dev \
+  libspeex-dev \
+  libssl-dev \
+  libtheora-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvo-amrwbenc-dev \
+  libvorbis-dev \
+  libwebp-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  libxvidcore-dev \
+  pkg-config \
+  texi2html \
+  zlib1g-dev
+
+# For 12.04
+# libx265 requires cmake version >= 2.8.8
+# 12.04 only have 2.8.7
+ubuntu_version=`lsb_release -rs`
+need_ppa=`echo $ubuntu_version'<=12.04' | bc -l`
+if [ $need_ppa -eq 1 ]; then
+    add-apt-repository ppa:roblib/ppa
+    apt-get -y update
+    apt-get -y install cmake
+fi
+
+./build.sh "$@"
